cd8da5fa14b06d1ba8da971cc571ec7901abe298
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-05-03 19:52:21 +0200

    almost done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const hbs = require('hbs');
 const path = require('path');
@@ -14,12 +15,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+/*app.get("/views/beers.hbs", (req, res) => 
+    res.sendFile(__dirname + "/views/checkbeers.hbs")
+);
+
+app.get("/views/randombeers.hbs", (req, res) => 
+    res.sendFile(__dirname + "/views/randombeers.hbs")
+);
+*/
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi =>
+      res.render('beers', { beers: beersFromApi }))
+
+    .catch(error => console.log(error))
+});
+
+/*punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    app.get('/random', (req, res) => {r
+      const randomIndex = Math.floor(Math.random() * beersList.length);
+      const dataToBeSent = { randomBeers: beersList[randomIndex] };
+
+      res.render('hbs-files/randombeers', dataToBeSent)
+  }
+*/
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then((randomBeerApi) => {
+    res.render('random-beer', {randomBeer: randomBeerApi});
+  })
+  .catch(error =>{
+    console.log(" no random beer", error)
+    res.send("we dont have any beers you like")
+  })
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
